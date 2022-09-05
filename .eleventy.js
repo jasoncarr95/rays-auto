884e9abdcd30baecb3b431d5ef3d2b5b48e13dda
jasoncarr95 <jason@jasoncarr.dev>
2022-09-05 19:22:34 -0400

    Add eleventy

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('img');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('js');
+    return {
+        passthroughFileCopy: true
+    };
+};
