8e4c6fd8576434703e44c24aec71027b143927cf
Jason Carr <jason@jasoncarr.dev>
2022-09-24 16:37:43 -0400

    Delete unused pages & add eleventy SSG

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('img');
-    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy('style');
     eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('rs-plugin');
+    eleventyConfig.addPassthroughCopy('fonts');
     return {
         passthroughFileCopy: true
     };
